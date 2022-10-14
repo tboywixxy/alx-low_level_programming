adf5277767a06faa2d5c77c8e6ca9ac44600c66e
tboywixxy <tboywixxy1@gmail.com>
2022-10-14 14:04:53 -0700

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+
 /**
  * main - Entry
- * Return: Always 0
+ * Return: Always 0 (success)
  */
 int main(void)
 
-	printf("with proper grammer, but the outcome is a peice of art,\n");
+	printf("with proper grammar, but the outcome is a peice of art, \n");
 	return (0);

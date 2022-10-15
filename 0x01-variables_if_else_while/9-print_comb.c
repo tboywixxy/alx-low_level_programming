a35b8cf403d61f263d5b5997706a43c017464721
tboywixxy <tboywixxy1@gmail.com>
2022-10-15 14:25:57 -0700

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#$include <stdio.h>
+
+/**
+ * main - prints all combination of single digits
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a + '0');
+		if (a < )
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}

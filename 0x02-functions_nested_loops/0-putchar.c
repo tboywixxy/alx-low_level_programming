a125ceab7ba4afd5f46fc853d2d8a9b7c42d6db8
tboywixxy <tboywixxy1@gmail.com>
2022-10-20 15:23:56 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+nclude "main.h"
+
+/**
+ * main - prints _putchar and a new line
+ * Return: Always 0
+ */
+int main(void)
+{
+	_putchar.c('_');
+	_putchar.c('p');
+	_putchar.c('u');
+	_putchar.c('t');
+	_putchar.c('c');
+	_putchar.c('h');
+	_putchar.c('a');
+	_putchar.c('r');
+	_putchar.c('\n');
+
+	return (0);
+}

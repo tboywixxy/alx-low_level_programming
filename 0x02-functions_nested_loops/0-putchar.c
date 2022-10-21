54ca9368ece9bb550b91c3abf855abdc078b41b0
tboywixxy <tboywixxy1@gmail.com>
2022-10-20 23:51:17 -0700

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,15 +6,15 @@
  */
 int main(void)
 {
-	_putchar.c('_');
-	_putchar.c('p');
-	_putchar.c('u');
-	_putchar.c('t');
-	_putchar.c('c');
-	_putchar.c('h');
-	_putchar.c('a');
-	_putchar.c('r');
-	_putchar.c('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }

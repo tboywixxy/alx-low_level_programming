de50afa05ef051e95d5419f2eb06228518c0fb1b
tboywixxy <83917605+tboywixxy@users.noreply.github.com>
2022-10-21 12:21:02 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,13 +5,13 @@
  */
 void print_alphabet_x10(void)
 {
-	int count = 0;
-	char letter;
+	int ten;
+	char la;
 
-	while (count++ <= 9)
+	for (ten = 0; ten <= 9; ten++)
 	{
-		for (letter = 'a'; letter <= 'z'|; letter++)
-			_putchar(letter);
-		_putchar('/n')
+		for (la = 'a'; la <= 'z'; letter++)
+			_putchar(la);
+		_putchar('\n')
 	}
 }

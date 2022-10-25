615551c0a6bdd7fd728ad1dda40cdbd9c5ee11db
tboywixxy <83917605+tboywixxy@users.noreply.github.com>
2022-10-25 13:54:12 +0100

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,1 +1,19 @@
-h
+#include "main.h"
+
+/**
+ * _strlen - returns the value of a string
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}

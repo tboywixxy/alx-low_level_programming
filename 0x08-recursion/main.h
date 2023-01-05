bca06af0f2d7642b6835012a5ac2bd51b3541420
tboywixxy <83917605+tboywixxy@users.noreply.github.com>
2023-01-05 11:44:56 +0100

    Create main.h

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,14 @@
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+<<<<<<< HEAD
+=======
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+int check_pal(char *s, int i, int len);
+>>>>>>> 2e0272e561547eee3612608a505aeb22c407afc9
